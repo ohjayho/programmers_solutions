b2f72f6349d08674993e548011deda2feef654fa
Jay Ho Oh <48760416+ohjayho@users.noreply.github.com>
2024-05-25 00:17:17 +0900

    [level 0] Title: 코드 처리하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
+++ JavaScript/프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
@@ -1,22 +1,17 @@
 function solution(code) {
+    let answer = '';
     let mode = 0;
-    let result = '';
     for(let i=0; i<code.length; i++){
         if(code[i]==1){
-            if(!mode){
-                mode = 1;
-            }else{
-                mode = 0;
-            }
+            mode = mode ? 0 : 1;
         } else {
-            if(i%2==0&&mode==0){
-                result += code[i];
+            if(i%2&&mode){
+                answer += code[i]
             }
-            if(i%2==1&&mode==1){
-                result += code[i];
+            if(!(i%2)&&!mode){
+                answer += code[i]
             }
         }
     }
-    return result ? result : 'EMPTY';  
-    }
-    
+    return answer ? answer : 'EMPTY';
+}

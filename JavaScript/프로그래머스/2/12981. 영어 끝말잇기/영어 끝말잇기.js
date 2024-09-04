0fb83bcc541b7d9a43324d50337963ed04d74720
Jay Ho Oh <48760416+ohjayho@users.noreply.github.com>
2024-09-04 22:09:36 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.28 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ JavaScript/프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,13 +1,14 @@
 function solution(n, words) {
-    savedWords = new Set();
-    prevWord = words[0][0]
-    for(i = 0; i<words.length; i++){
-        word = words[i];
-        if(word[0] !== prevWord || savedWords.has(word)){
-            return [i%n+1, Math.floor(i/n)+1];
+    // const savedWords = new Set();
+    const savedWords = [];
+    let prevLetter = words[0][0];
+    for(let i=0; i<words.length; i++){
+        let word = words[i];
+        if(word[0] !== prevLetter || savedWords.includes(word)){
+            return [i%n+1,Math.floor(i/n)+1];
         }
-        savedWords.add(word);
-        prevWord = word.slice(-1);
+        savedWords.push(word);
+        prevLetter = word.slice(-1);
     }
-    return [0,0]
+    return [0,0];
 }

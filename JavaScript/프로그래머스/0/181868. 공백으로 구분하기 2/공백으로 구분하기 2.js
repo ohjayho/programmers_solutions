2ea168efa62af352dd0557f0256bb72123dbad9c
Jay Ho Oh <48760416+ohjayho@users.noreply.github.com>
2024-10-28 02:36:10 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
+++ JavaScript/프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
@@ -1,19 +1,3 @@
 function solution(my_string) {
-    let result = '';
-    let resultArray = [];
-    for(let i of my_string){
-        if(i===' '){
-            if(result.length){
-                resultArray.push(result);
-                result = '';
-            }
-        }else{
-            result += i;
-            console.log(result)
-        }
-    }
-    if(result.length){
-        resultArray.push(result);
-    }
-    return resultArray;
+    return my_string.replace(/\s+/g, ' ').trim().split(' ');
 }

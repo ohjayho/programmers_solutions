315ad2fdc7ab14950b37ea822d96fbd9326de73b
Jay Ho Oh <48760416+ohjayho@users.noreply.github.com>
2024-10-30 23:35:35 +0900

    [level 3] Title: 입국심사, Time: 268.36 ms, Memory: 43.6 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/3/43238. 입국심사/입국심사.js
@@ -0,0 +1,16 @@
+function solution(n, times) {
+    const sorted = times.sort((a, b) => a-b);
+    let start = 1;
+    let end = sorted[sorted.length-1] * n;
+    
+    while (start <= end) {
+        const mid = Math.floor((start+end)/2); 
+        const sum = times.reduce((acc, cur) => acc+Math.floor(mid/cur), 0);
+        if (sum<n) {
+            start = mid+1
+        } else {
+            end = mid-1;
+        }
+    }
+    return start;
+}

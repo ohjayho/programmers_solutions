4480f793d8af04f94e5fe38b8c4afc9aa2e3d03f
Jay Ho Oh <48760416+ohjayho@users.noreply.github.com>
2025-01-01 22:09:28 +0900

    [level 0] Title: 약수 구하기, Time: 0.50 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/120897. 약수 구하기/약수 구하기.js
@@ -0,0 +1,9 @@
+function solution(n) {
+    const result = [];
+    for(let i=n; i>0; i--){
+        if(n%i===0){
+            result.push(i);
+        }
+    }
+    return result.sort((a,b)=>a-b);
+}

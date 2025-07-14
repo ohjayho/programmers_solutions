2230c4ee4f8c0ed444b18ce080880017335e961a
Jay Ho Oh <48760416+ohjayho@users.noreply.github.com>
2025-07-15 07:35:04 +0900

    [level 0] Title: 모스부호 (1), Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,14 @@
+function solution(letter) {
+    const morseChart = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+};
+    let answer = '';
+    letter.split(' ').forEach((morse)=>{
+        answer += morseChart[morse];
+    })
+    return answer;
+}
